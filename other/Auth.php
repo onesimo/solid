ec621e9296b34d8626328af0d73055a87d3b0f6b
Onesimo Batista <onesimobatista@gmail.com>
2018-03-04 15:17:06 +0000

    add classes Auth and AuthAPI ¬

---
+++ other/Auth.php
@@ -0,0 +1,39 @@
+<?php
+
+class Auth
+{
+	public function authenticate($user, $password)
+	{
+		if ($user == 'onesimo' and $password == '123')
+		{
+			return true;
+		}
+		return false;
+	}
+}
+
+class AuthApi extends Auth
+{
+	public function authenticate($user, $password)
+	{
+		if($user == 'onesimo' and $password == '123')
+		{
+			return [
+				'status' => 'ok'
+			];
+		}
+		return [
+			'status' => 'fail'
+		];
+	}
+}
+
+$auth1 = new Auth;
+$auth2 = new AuthApi;
+$auth1 = $auth1->authenticate('onesimo', '123');
+$auth2 = $auth2->authenticate('onesimo', '123');
+
+
+if($auth1){
+	echo 'logado';
+}
